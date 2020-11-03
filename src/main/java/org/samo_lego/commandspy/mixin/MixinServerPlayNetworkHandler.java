a720e4b9ab976322ffe8a2ccdbb3b230d6e4d9ba
samo_lego <34912839+samolego@users.noreply.github.com>
2020-11-03 22:29:33 +0100

    Small update + version bump

--- src/main/java/org/samo_lego/commandspy/mixin/MixinServerPlayNetworkHandler.java
+++ src/main/java/org/samo_lego/commandspy/mixin/MixinServerPlayNetworkHandler.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.core.lookup.StrSubstitutor;
 import org.samo_lego.commandspy.CommandSpy;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,6 +19,9 @@ import java.util.Map;
 public abstract class MixinServerPlayNetworkHandler {
 	@Shadow
 	public ServerPlayerEntity player;
+
+	@Unique
+	private final Map<String, String> valuesMap = new HashMap<>();
 
 	// Injection for player chatting
 	@Inject(
@@ -41,7 +45,6 @@ public abstract class MixinServerPlayNetworkHandler {
 			String uuid = player.getUuidAsString();
 
 			// Saving those to hashmap for fancy printing with logger
-			Map<String, String> valuesMap = new HashMap<>();
 			valuesMap.put("playername", playername);
 			valuesMap.put("uuid", uuid);
 			valuesMap.put("command", command);
@@ -49,9 +52,6 @@ public abstract class MixinServerPlayNetworkHandler {
 			StrSubstitutor sub = new StrSubstitutor(valuesMap);
 			// Logging to console
 			CommandSpy.LOGGER.info(sub.replace(message));
-
-			// Clearing the left-over map
-			valuesMap.clear();
 		}
 	}
 }
